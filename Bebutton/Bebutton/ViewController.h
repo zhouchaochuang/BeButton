e5d497b8f6d119454e409026c209c4ec213e6256
zhouchaochuang <zhouchaochuang@163.com>
2018-09-15 11:55:25 +0800

    First Commit

---
+++ Bebutton/Bebutton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bebutton
+//
+//  Created by wallace on 2018/9/15.
+//  Copyright © 2018年 wallace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Bebutton/Bebutton/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Bebutton
+//
+//  Created by wallace on 2018/9/15.
+//  Copyright © 2018年 wallace. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
